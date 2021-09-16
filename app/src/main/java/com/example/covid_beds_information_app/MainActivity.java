c95bc6f18905598e492358d0f63024abe1d0ba99
Ashish <ashish.khare@somaiya.edu>
2021-09-16 14:16:19 +0530

    initial commit

---
+++ app/src/main/java/com/example/covid_beds_information_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.covid_beds_information_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
